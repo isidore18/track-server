6d41ef15060b906282871cf75c864472da5700e5
adrien <adrien@ylly.fr>
2020-04-13 11:45:07 +0200

    first commit

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const User = mongoose.model('User');
+const jwt = require('jsonwebtoken');
+
+router.post('/signup', async(req, res) => {
+    const { email, password} = req.body;
+    console.log(req.body);
+    
+    try {
+        const user = new User({ email, password});
+        await user.save();
+
+        const token = jwt.sign(
+            {userId: user._id},
+            'MY_SECRET_KEY'
+        );
+        res.send({token});
+    } catch(err) {
+        return res.status(422).send(err);
+    }
+})
+
+router.post('/signin', async(req, res) => {
+    const {email, password}= req.body
+
+    if (!email || !password) {
+        return res.status(422).send({error: 'Must provide email and password'})
+    }
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+        return res.status(422).send({error: 'Invalid password or email'})
+    }
+
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
+        res.send({token})
+    } catch (err) {
+        return res.status(422).send({error: 'Invalid password or email'})
+    }
+})
+
+module.exports = router;
